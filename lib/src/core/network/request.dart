12f7669122c07054e22abd59f6b2b2c3159e1aeb
App Dev (Al Talha) <app.dev@sgclbd.com>
2025-12-10 17:43:39 +0600

    refactor: Ensure request body values are strings
    This commit updates the network request handling to ensure all values in the request body are converted to strings before being assigned to `request.fields`.

--- lib/src/core/network/request.dart
+++ lib/src/core/network/request.dart
@@ -159,7 +159,7 @@ Future<String> oSNetworkRequestWithFile(
 
   if (body != null) {
     body.forEach((key, value) {
-      request.fields[key] = value;
+      request.fields[key] = value.toString();
     });
   }
 
